965045cd528e7d08b6ce7c36af1909ef8a246967
Mandy Tang <mandytang@Mandys-MacBook-Pro.local>
2018-04-02 12:54:27 +1000

    marginally better version of ratings but still broken

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -9,7 +9,7 @@ class DesignsController < ApplicationController
 
   def create
     design = Design.create design_params
-    @design_rating = design.design_ratings.new design_params
+    @design_rating = DesignRating.new design_params
     @design_rating.save
     redirect_to design
   end
@@ -53,7 +53,7 @@ class DesignsController < ApplicationController
 
 private
 def design_params
-  params.require(:design).permit(:name, :instructions, :image, :difficulty, :creator_id, :rating_value, :rating_id, :user_id, design_ratings_attributes: [:rating_value])
+  params.require(:design).permit(:name, :instructions, :image, :difficulty, :creator_id, :rating_id, :user_id, design_ratings_attributes: [:rating_value])
 end
 
 end
