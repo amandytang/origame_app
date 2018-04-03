c97d095a68153d6f2299f4585c9baf95b6643005
Mandy Tang <mandytang@Mandys-MacBook-Pro.local>
2018-04-03 14:38:35 +1000

    deploying to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  post '/rate' => 'rater#create', :as => 'rate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'pages#home'
@@ -8,14 +6,19 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/login' => 'sessions#destroy'
+
+  resources :creators
   resources :designs
   resources :designs do
     put :favourite, on: :member
   end
-  resources :designs do
-put :rating, on: :member
-end
   get '/favourites' => 'designfavourites#index'
-  resources :creators
+
+  resources :designs
+    put '/designs/:id/favourite' => 'designs#favourite' as favourite_design
+
+  resources :designs do
+    put :rating, on: :member
+  end
 
 end
